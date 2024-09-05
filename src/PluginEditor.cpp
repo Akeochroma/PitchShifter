15aa77517e89fa40213c8c637dab92b63215b797
Akeochroma <131672353+Akeochroma@users.noreply.github.com>
2024-09-06 01:12:45 +0300

    Add files via upload

---
+++ src/PluginEditor.cpp
@@ -0,0 +1,48 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+VdlpitchAudioProcessorEditor::VdlpitchAudioProcessorEditor (VdlpitchAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    dTimeSlider.setSliderStyle (juce::Slider::SliderStyle::RotaryVerticalDrag);
+    //setTextBoxStyle
+    //dTimeSlider.setRange (0, 2000, 1);
+    addAndMakeVisible (dTimeSlider);
+    
+    dTimeAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment> (audioProcessor.getAPVTS(), "TIME", dTimeSlider);
+    
+    setSize (400, 300);
+    
+    
+    
+    
+}
+
+VdlpitchAudioProcessorEditor::~VdlpitchAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void VdlpitchAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void VdlpitchAudioProcessorEditor::resized()
+{
+    dTimeSlider.setBoundsRelative (0.2, 0.2, 0.6, 0.6);
+}
---
+++ src/PluginEditor.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class VdlpitchAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    VdlpitchAudioProcessorEditor (VdlpitchAudioProcessor&);
+    ~VdlpitchAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    
+    
+    juce::Slider dTimeSlider;
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> dTimeAttach;
+    
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    VdlpitchAudioProcessor& audioProcessor;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VdlpitchAudioProcessorEditor)
+};
